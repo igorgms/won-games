dc10f61ab10bc55c915297f8360f4fe6b5e5c704
Igor Gomes <igor.firmino@corebiz.ag>
2022-03-31 19:43:40 -0300

    first commit

---
+++ api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0c7089054c9928e8c3da2a5eb436e309'),
+    },
+  },
+});
