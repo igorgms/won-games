a197402689ef9ff9d75432cbb1eed7671392de3c
Igor Gomes <igorgmsf@gmail.com>
2022-04-04 23:08:11 -0300

    add crawler for game category populate

--- api/api/game/services/game.js
+++ api/api/game/services/game.js
@@ -1,8 +1,33 @@
-'use strict';
-
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+const axios = require("axios");
+
+async function getGameInfo(slug) {
+  const jsdom = require("jsdom");
+  const { JSDOM } = jsdom;
+  const body = await axios.get(`https://www.gog.com/game/${slug}`);
+  const dom = new JSDOM(body.data);
+
+  const description = dom.window.document.querySelector(".description");
+
+  return {
+    rating: "BR0",
+    short_description: description.textContent.trim().slice(0, 160),
+    description: description.innerHTML,
+  };
+}
+
+module.exports = {
+  populate: async (params) => {
+    const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
+
+    const {
+      data: { products },
+    } = await axios.get(gogApiUrl);
+
+    console.log(await getGameInfo(products[1].slug));
+  },
+};
