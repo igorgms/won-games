b736a94044e23037e12cbf8e997aae47d23036c2
Igor Gomes Firmino <igorgmsf@gmail.com>
2022-05-30 22:16:17 -0300

    finish highlight component

---
+++ client/src/components/Highlight/stories.tsx
@@ -0,0 +1,27 @@
+import { Story, Meta } from '@storybook/react'
+import Highlight, { HighlightProps } from '.'
+
+export default {
+  title: 'Highlight',
+  component: Highlight,
+  args: {
+    title: 'Read Dead its back',
+    subtitle: 'Come see John’s new adventures',
+    buttonLabel: 'Buy now',
+    buttonLink: '/rdr2',
+    backgroundImage: '/img/red-dead-img.jpg',
+    floatImage: '/img/red-dead-float.png'
+  }
+} as Meta
+
+export const Default: Story<HighlightProps> = (args) => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Highlight {...args} />
+  </div>
+)
+
+export const WithFloatImage: Story<HighlightProps> = (args) => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Highlight {...args} />
+  </div>
+)
